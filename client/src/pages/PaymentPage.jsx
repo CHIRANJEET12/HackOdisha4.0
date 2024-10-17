1a21ecd214d12a87ff8fea45cae70fb06b8d2139
Sowmik Dey <sowmikdey23@gmail.com>
2024-10-17 19:46:30 +0530

    Payment added

---
+++ client/src/pages/PaymentPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { PaymentButton } from './PaymentButton';
+import '../css/PaymentPage.css'
+
+export const PaymentPage = () => {
+  const location = useLocation();
+  const { sellerId, amount } = location.state || {};
+
+  if (!sellerId || !amount) {
+    return <p>Invalid payment details. Please try again.</p>;
+  }
+
+  return (
+    <div className='pay-button-container'>
+      <h1 className=''>Proceed to Payment</h1>
+      <PaymentButton sellerId={sellerId} amount={amount} />
+    </div>
+  );
+};
